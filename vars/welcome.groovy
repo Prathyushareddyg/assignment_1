60d7fe1fab9b42c667de4d2d72e869f559feff10
Prathyushareddyg <92011622+Prathyushareddyg@users.noreply.github.com>
2022-04-07 16:51:31 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,3 @@
-def call(name){
-  echo "hey ${name}"
+def call(AWS_ACCOUNT_ID,AWS_DEFAULT_REGION){
+ sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
 }
